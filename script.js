e7e9812f0001fd46ab1b25551280a2f61d02bf1d
Tedyzcs <157959708+Tedyzcs@users.noreply.github.com>
2024-12-29 09:56:58 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function openSidebar() {
+    document.getElementById("mySidebar").style.left = "0";
+  }
+  
+  function closeSidebar() {
+    document.getElementById("mySidebar").style.left = "-250px";
+  }
+  
